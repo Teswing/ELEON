d4ac79f265ffbb1bdd4f14b2284a7879bd079438
GlintMain <krendklud@gmail.com>
2023-06-09 22:50:41 +0300

    Теперь прослушанные уведомления сохраняются локально + Оптимизирован и исправлен RecyclerView

--- app/src/main/java/com/teswing/eleon/NotificationListActivity.java
+++ app/src/main/java/com/teswing/eleon/NotificationListActivity.java
@@ -2,7 +2,6 @@ package com.teswing.eleon;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -40,7 +39,7 @@ public class NotificationListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.e(TAG, "ACTIVITY (RE-)CREATED");
+//        Log.e(TAG, "ACTIVITY (RE-)CREATED");
         binding = ActivityRecycleViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         notificationListViewModel = new ViewModelProvider(this).get(NotificationListViewModel.class);
@@ -53,9 +52,9 @@ public class NotificationListActivity extends AppCompatActivity {
         notificationListViewModel.getNotificationsLiveData().observe(this, new Observer<List<Notification>>() {
             @Override
             public void onChanged(List<Notification> notificationList) {
-                    Log.i("NOTIFICATION LD", "I'm CHANGED");
+//                    Log.i("NOTIFICATION LD", "I'm CHANGED");
                     adapter.submitList(notificationList);
-                    Log.i("ON_UPDATE", String.valueOf(adapter.getItemCount()));
+//                    Log.i("ON_UPDATE", String.valueOf(adapter.getItemCount()));
                     recyclerView.smoothScrollToPosition(adapter.getItemCount());
             }
         });
@@ -75,6 +74,7 @@ public class NotificationListActivity extends AppCompatActivity {
             }
         });
         recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
         registerForContextMenu(recyclerView);
     }
 
--- app/src/main/java/com/teswing/eleon/NotificationListener.java
+++ app/src/main/java/com/teswing/eleon/NotificationListener.java
@@ -1,41 +1,34 @@
 package com.teswing.eleon;
 
 import android.app.Notification;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
+
+import androidx.lifecycle.ViewModelProvider;
 
 public class NotificationListener extends NotificationListenerService {
 
     private static final String TAG = "Notification Listener";
 
+    NotificationListViewModel notificationListViewModel;
+
+
+    // Пока на основном потоке
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
-        Notification notification = sbn.getNotification();
-        Bundle extras = notification.extras;
-        String title = extras.getString(android.app.Notification.EXTRA_TITLE);
-        String text = extras.getString(android.app.Notification.EXTRA_TEXT);
-        String icon = extras.getString(Notification.EXTRA_SMALL_ICON);
-        Log.e(TAG, "Notification POSTED");
-        try {
-            final PackageManager pm = getApplicationContext().getPackageManager();
-            Log.i(TAG, "App: \n" + (pm.getApplicationLabel(pm.getApplicationInfo(sbn.getPackageName(), 0))));
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.i(TAG, "App: \n" + sbn.getPackageName());
+        if (!"ranker_group".equals(sbn.getTag())) {
+            Notification notification = sbn.getNotification();
+            Bundle extras = notification.extras;
+            String title = extras.getString(Notification.EXTRA_TITLE);
+            String text = extras.getString(Notification.EXTRA_TEXT);
+            notificationListViewModel.insert(new com.teswing.eleon.Notification(title, text));
+            Log.e(TAG, "Notification POSTED");
+//            Log.i(TAG, "Title: \n" + title);
+//            Log.i(TAG, "Message: \n " + text);
         }
-        Log.i(TAG, "Title: \n" + title);
-        Log.i(TAG, "Message: \n " + text);
-        Log.i(TAG, "Icon: \n " + icon);
-        Log.i(TAG, "Time: \n " + sbn.getNotification().when);
-//        Icon icon = notification.getSmallIcon();
-//        Intent intent = new Intent(Constants.ACTION_NOTIFICATION_RECEIVER);
-//        intent.putExtra("Notification", sbn.getNotification());
-//        sendBroadcast(intent);
     }
 
     @Override
@@ -43,34 +36,12 @@ public class NotificationListener extends NotificationListenerService {
         super.onNotificationRemoved(sbn);
     }
 
-/*    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(TAG, "Listener STARTED not connected ?");
-
-        Intent switchIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
-                switchIntent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-
-        Notification notification = new NotificationCompat.Builder(this, NF_CHANNEL_ID)
-                .setContentTitle("Notification Listener")
-                .setContentText("I'm listening")
-                .setSmallIcon(R.drawable.baseline_remove_red_eye_24)
-                .setContentIntent(pendingIntent)
-                .addAction(R.drawable.baseline_remove_red_eye_24, "Butoon", pendingIntent)
-                .setOngoing(true)
-                .build();
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-//        notificationManager.notify(1, notification);
-        startForeground(1, notification);
-
-        return START_NOT_STICKY;
-    }*/
 
     @Override
     public void onCreate() {
         super.onCreate();
+        notificationListViewModel = ViewModelProvider.AndroidViewModelFactory
+                .getInstance(getApplication()).create(NotificationListViewModel.class);
         Log.i(TAG, "NOTIFICATION LISTENER CREATED");
     }
 
@@ -80,8 +51,28 @@ public class NotificationListener extends NotificationListenerService {
         Log.i(TAG, "Listener Destroyed");
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return super.onBind(intent);
-    }
+    /*    @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            Log.i(TAG, "Listener STARTED not connected ?");
+
+            Intent switchIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                    switchIntent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+            Notification notification = new NotificationCompat.Builder(this, NF_CHANNEL_ID)
+                    .setContentTitle("Notification Listener")
+                    .setContentText("I'm listening")
+                    .setSmallIcon(R.drawable.baseline_remove_red_eye_24)
+                    .setContentIntent(pendingIntent)
+                    .addAction(R.drawable.baseline_remove_red_eye_24, "Butoon", pendingIntent)
+                    .setOngoing(true)
+                    .build();
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+    //        notificationManager.notify(1, notification);
+            startForeground(1, notification);
+
+            return START_NOT_STICKY;
+        }*/
 }
